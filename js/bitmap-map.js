3f34cc2088d7ede958ed9dd3bac20dee009fd4be
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2016-03-09 17:45:09 +0100

    Very buggy version with a layer that works

--- js/bitmap-map.js
+++ js/bitmap-map.js
@@ -15,7 +15,7 @@ function createMap(embed)
 	tiledir = "http://tiles.mountbatten.nl/";
 	// available map rendering styles
 	availableStyles = new Array("buildings", "architecture", "all");
-        defaultStyle = "buildings";
+        defaultStyle = "architecture";
 	if (params['offset'] != null)
 		offset = params['offset'];
 	else
@@ -77,6 +77,91 @@ function createMap(embed)
                 zoomOffset: 0,
 	}).addTo(map);
 
+	
+	var MVTSource = new L.TileLayer.MVTSource({
+//	    url: "http://spatialserver.spatialdev.com/services/vector-tiles/GAUL_FSP/{z}/{x}/{y}.pbf",
+	    url: tiledir+defaultStyle+'/{z}/{x}/{y}.mvt',
+	    debug: true,
+	    clickableLayers: [defaultStyle],
+	    getIDForLayerFeature: function(feature) {
+	      return feature.properties.id;
+	    },
+
+	    /**
+	     * The filter function gets called when iterating though each vector tile feature (vtf). You have access
+	     * to every property associated with a given feature (the feature, and the layer). You can also filter
+	     * based of the context (each tile that the feature is drawn onto).
+	     *
+	     * Returning false skips over the feature and it is not drawn.
+	     *
+	     * @param feature
+	     * @returns {boolean}
+	     */
+	    filter: function(feature, context) {
+	      if (feature.layer.name === defaultStyle) {
+	        return true;
+	      }
+	      return false;
+	    },
+
+	    style: function (feature) {
+	      var style = {};
+
+	      var type = feature.type;
+	      switch (type) {
+	        case 1: //'Point'
+	          style.color = 'rgba(49,79,79,1)';
+	          style.radius = 5;
+	          style.selected = {
+	            color: 'rgba(255,255,0,0.5)',
+	            radius: 6
+	          };
+	          break;
+	        case 2: //'LineString'
+	          style.color = 'rgba(161,217,155,0.8)';
+	          style.size = 3;
+	          style.selected = {
+	            color: 'rgba(255,25,0,0.5)',
+	            size: 4
+	          };
+	          break;
+	        case 3: //'Polygon'
+	          style.color = fillColor;
+	          style.outline = {
+	            color: strokeColor,
+	            size: 1
+	          };
+	          style.selected = {
+	            color: 'rgba(255,140,0,0.3)',
+	            outline: {
+	              color: 'rgba(255,140,0,1)',
+	              size: 2
+	            }
+	          };
+	          break;
+	      }
+	      
+	      switch (feature.properties.architecture_style) {
+	      case "modernism":
+	          style.color = "blue";
+	          break;
+	      
+	    };
+	      return style;
+	    }
+
+	  });
+	//  debug.mvtSource = mvtSource;
+
+	  //Globals that we can change later.
+	  var fillColor = 'rgba(149,139,255,0.4)';
+	  var strokeColor = 'rgb(20,20,20)';
+
+	  //Add layer
+	 map.addLayer(MVTSource);
+
+	
+	
 	if (!embed)
 	{
 		map.on('zoomend', function(e)
--- js/kothic/kothic-leaflet.js
+++ js/kothic/kothic-leaflet.js
@@ -10,7 +10,8 @@ L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
                      ' Rendering by <a href="http://github.com/kothic/kothic-js">Kothic JS</a>',
         async: true,
         buffered: false,
-        styles: MapCSS.availableStyles
+        styles: MapCSS.availableStyles,
+        granularity: 1000,
     },
 
     initialize: function(url,options) {
@@ -21,7 +22,7 @@ L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
         this._scripts = {};
         this._debugMessages = [];
 
-        window.onKothicDataResponse = L.Util.bind(this._onKothicDataResponse, this);
+      //  window.onKothicDataResponse = L.Util.bind(this._onKothicDataResponse, this);
     },
 
     _onKothicDataResponse: function(data, zoom, x, y) {
@@ -37,8 +38,8 @@ L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
         function onRenderComplete() {
             layer.tileDrawn(canvas);
 
-            document.getElementsByTagName('head')[0].removeChild(layer._scripts[key]);
-            delete layer._scripts[key];
+      //      document.getElementsByTagName('head')[0].removeChild(layer._scripts[key]);
+        //    delete layer._scripts[key];
         }
 
         this._invertYAxe(data);
@@ -59,15 +60,29 @@ L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
     },
 
     drawTile: function(canvas, tilePoint, zoom) {
+        return;
         var zoomOffset = this.options.zoomOffset,
             key = [(zoom - zoomOffset), tilePoint.x, tilePoint.y].join('/'),
             url=this._url.replace('{x}',tilePoint.x).
                     replace('{y}',tilePoint.y).
                     replace('{z}',zoom-zoomOffset);
         this._canvases[key] = canvas;
-        this._scripts[key] = this._loadScript(url);
+        //this._scripts[key] = this._loadScript(url);
+        $.getJSON({
+            dataType: "json",
+            url: url,
+            x: tilePoint.x,
+            y: tilePoint.y,
+            zoom: zoom-zoomOffset,
+            canvas: canvas,
+            layer: this,
+            key: key,
+            success: function(data){
+                this.layer._renderTile(data,this.x,this.y,this.zoom,this.canvas,this.layer);
+            },
+          });
     },
-
+    
     enableStyle: function(name) {
         if (MapCSS.availableStyles.indexOf(name) >= 0 && this.options.styles.indexOf(name) < 0) {
             this.options.styles.push(name);
@@ -93,10 +108,33 @@ L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
         }
     },
 
+    _renderTile: function (data,x,y,zoom,canvas,layer) {
+        function onRenderComplete() {
+            layer.tileDrawn(canvas);
+
+      //      document.getElementsByTagName('head')[0].removeChild(layer._scripts[key]);
+        //    delete layer._scripts[key];
+        }
+        //TODO if featurecollection
+        data.granularity = this.options.tileSize;
+        
+        this._invertYAxe(data);
+
+        var styles = this.options.styles;
+
+        Kothic.render(canvas, data, zoom + this.options.zoomOffset, {
+            styles: styles,
+            locales: ['be', 'ru', 'en'],
+            onRenderComplete: this._onRenderTileComplete
+        });
+    },
+
     _invertYAxe: function(data) {
         var type, coordinates, tileSize = data.granularity, i, j, k, l, feature;
         for (i = 0; i < data.features.length; i++) {
             feature = data.features[i];
+            feature.coordinates = feature.geometry.coordinates;
+            feature.type = feature.geometry.type;
             coordinates = feature.coordinates;
             type = data.features[i].type;
             if (type === 'Point') {
@@ -129,11 +167,119 @@ L.TileLayer.Kothic = L.TileLayer.Canvas.extend({
         }
     },
 
-    _loadScript: function(url) {
-        var script = document.createElement('script');
-        script.src = url;
-        script.charset = 'utf-8';
-        document.getElementsByTagName('head')[0].appendChild(script);
-        return script;
+    _get_sphmerc_bbox:function (x,y,z) {
+        var HALF_EARTH = 20037508.34;
+        var EARTH =40075016.68;
+        bbox=array();
+        metresInTile = EARTH /Math.pow(2,z);
+        bbox[0] = (x*$metresInTile)-HALF_EARTH - 0.01;
+        bbox[3] = HALF_EARTH-(y*metresInTile) - 0.01;
+        bbox[2] = bbox[0]+metresInTile + 0.01;
+        bbox[1] = bbox[3]-metresInTile + 0.01;
+        return bbox;
+    },
+    
+    _sphmerc_to_ll: function (x,y) {
+        var value = array();
+        value['lon'] = sphmerc_to_lon(x);
+        value['lat'] = sphmerc_to_lat(y);
+        return value;
+    },
+    
+    _sphmerc_to_lon: function (m) {
+        var HALF_EARTH = 20037508.34;
+        return (m/HALF_EARTH) * 180.0;
+    },
+    
+    _kothicAdjust: function(f, bbox)
+    {
+        granularity = this.options.granularity;
+        factor = granularity / (bbox[2]-bbox[0]);
+        coords=array();
+        switch(f["type"])
+        {
+            case "Point":
+                x = ((f["coordinates"][0] - bbox[0]) * factor);
+                y = ((f["coordinates"][1] - bbox[1]) * factor);
+            coords = array(x,y);
+            break;
+            case "LineString":
+                for(i=0; i<count(f["coordinates"]); i++)
+                {
+                    x =  (int)    
+                    round((f["coordinates"][i][0]-bbox[0]) * factor);
+                    y =     (int)
+                    round((f["coordinates"][i][1]-bbox[1]) * factor);
+                    // coords of (0,0) seem to  screw up rendering
+                    x=(x==0)?1:x;
+                    y=(y==0)?1:y;
+    
+                    x=(x==granularity)?granularity-1:x;
+                    y=(y==granularity)?granularity-1:y;
+                    if(x>=0 && y>=0 && x<=granularity && 
+                        y<=granularity)
+            if(true)
+                    {
+                //FIXME
+                           coords = array(x,y);
+                    }
+                }
+                break;
+            case "MultiLineString":
+            case "Polygon":
+                for(i=0; i<count(f["coordinates"]); i++)
+                {
+                    coords[i]=array();
+                    for(j=0; j<count(f["coordinates"][i]); j++)
+                    {
+                        x =  (int)    
+                        round((f["coordinates"][i][j][0]-
+                        bbox[0]) * factor);
+                        y =     (int)
+                        round((f["coordinates"][i][j][1]-
+                        bbox[1]) * factor);
+                        // coords of (0,0) seem to screw up rendering
+                        if($f["type"]=="MultiLineString")
+                        {
+                            x=(x==0)?1:x;
+                            y=(y==0)?1:y;
+                            x=(x==granularity)?granularity-1:x;
+                            y=(y==granularity)?granularity-1:y;
+                        }
+            //FIXME
+                        coords[i] = array(x,y);
+                    }
+                }
+                break;
+            case "MultiPolygon":
+                for(i=0; i<f["coordinates"].length; i++)
+                {
+                    coords[i]= new Array;
+                    for(j=0; j<count(f["coordinates"][i]); j++)
+                    {
+                        coords[i][j]=array();
+                        for($k=0; $k<count(f["coordinates"][i][j]); $k++)
+                        {
+                            x =  (int)    
+                            round((f["coordinates"][i][j][$k][0]-
+                            bbox[0]) * 
+                            factor);
+                            y =     (int)
+                            round((f["coordinates"][i][j][$k][1]-
+                            bbox[1]) * 
+                            factor);
+                // coords of (0,0) screw up rendering
+                            x=(x==0)?1:x;
+                            y=(y==0)?1:y;
+                            x=(x==granularity)?granularity-1:x;
+                            y=(y==granularity)?granularity-1:y;
+                            //FIXME
+                            coords[i][j] = array(x,y);
+                        }
+                    }
+                }
+                break;
+        }
+        return coords;
     }
 });
